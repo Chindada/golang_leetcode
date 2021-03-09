281873d08f48a3f06e333b23ac0f7c6eb89d2c75
TimHsu@MBP <maochindada@gmail.com>
2021-03-10 00:01:50 +0800

    Rename

---
+++ Failed/497/main.go
@@ -0,0 +1,29 @@
+package main
+
+func main() []int {
+	var rec [][]int
+	input := Constructor(rec)
+	return input.Pick()
+}
+
+// Solution Solution
+type Solution struct {
+}
+
+// Constructor Constructor
+func Constructor(rects [][]int) Solution {
+	var ans Solution
+	return ans
+}
+
+// Pick Pick
+func (u *Solution) Pick() []int {
+	var pick []int
+	return pick
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * obj := Constructor(rects);
+ * param_1 := obj.Pick();
+ */
