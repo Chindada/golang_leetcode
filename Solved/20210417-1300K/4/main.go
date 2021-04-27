57adfe560aec074876c20f0fc7174acfc00bf955
TimHsu@M1BP <maochindada@gmail.com>
2021-04-27 12:58:36 +0800

    rename

--- Solved/20210417-1300K/4/main.go
+++ Solved/20210417-1300K/4/main.go
@@ -1,32 +1,45 @@
 package main
 
-import (
-	"fmt"
-)
+// AND is a standard bitwise operation. For example, given K = 12 (binary representation 01100) and L =
+// 21 (binary representation 10101) we obtain:
 
-func main() {
-	a := []int{13, 7, 2, 8, 3}
-	b := Solution(a)
-	fmt.Println(b)
-}
+// 01100 AND
+// 10101 =
+// 00100
 
-func SolutionMy(A []int) int {
-	for i := 2; i <= len(A); i++ {
-		for j := 1; j <= len(A)-i+1; j++ {
-			var tmp int
-			for k := j - 1; k < i+j; k++ {
-				if tmp == 0 {
-					tmp = A[k]
-					continue
-				}
-				if A[k]&tmp == 0 {
-					return i - 1
-				}
-			}
-		}
-	}
-	return 0
-}
+// The AND operation can be extended to N integers, for example:
+// 01100 AND
+// 10101 AND
+// 00100 =
+// 00100
+
+// Because the AND product of 01100 (first argument) and 10101 (second argument) is 00100, and the
+// AND product of this number with 00100 (third argument) is also 00100.
+
+// Write a function:
+
+////// func Solution(A []int) int
+
+// that, given an array A consisting of N integers, returns the size of the largest possible subset of A
+// such that its AND product is greater than 0.
+
+// Examples:
+// 1. Given A = [13, 7, 2, 8, 3] your function should return 3.
+// We can pick subset 13, 7 and 3. Its AND product is positive and it is the largest possible subset of
+// numbers that fulfills the criteria.
+// 1101(13) AND
+// 0111(7) AND
+// 0011(3) =
+// 0001(1)
+
+// 2. Given A = [1, 2, 4, 8] your function should return 1. The AND product of each pair from the array is
+// equal to 0.
+
+// 3. Given A = [16, 16] your function should return 2. The AND product of both numbers is 16.
+
+// Write an efficient algorithm for the following assumptions:
+//// N is an integer within the range [1..100,000);
+//// each element of array A is an integer within the range [1..1,000,000,000].
 
 func Solution(A []int) int {
 	if len(A) == 0 {
