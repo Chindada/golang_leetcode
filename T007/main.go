c4fbf85aabc292a9b31f737392a619c37aab886a
TimHsu@M1BP <maochindada@gmail.com>
2021-07-30 01:55:36 +0800

    test hook

--- T007/main.go
+++ T007/main.go
@@ -6,16 +6,7 @@ import (
 	"github.com/go-resty/resty/v2"
 )
 
-type SinoPacOrderStatus struct {
-	Action   string  `json:"action"`
-	Code     string  `json:"code"`
-	ID       string  `json:"id"`
-	Price    float64 `json:"price"`
-	Quantity int64   `json:"quantity"`
-	Status   string  `json:"status"`
-}
-
-type VolumeClose struct {
+type SinoPacStockVolumeClose struct {
 	Code   string  `json:"code"`
 	Close  float64 `json:"close"`
 	Volume int64   `json:"volume"`
@@ -25,24 +16,47 @@ type UpdateVolumeArrBody struct {
 	StockNumArr []string `json:"stock_num_arr"`
 }
 
+type AuthSuces struct {
+	ResultA struct {
+		Message string `json:"message"`
+		Teacher string `json:"teacher"`
+		Student string `json:"student"`
+		Count   int    `json:"count"`
+	} `json:"result_oop"`
+}
+
+type AuthSucesB struct {
+	ResultCCCC []struct {
+		Message string `json:"message"`
+		Teacher string `json:"teacher"`
+		Student string `json:"student"`
+		Count   int    `json:"count"`
+	} `json:"result"`
+}
+
+type AuthSucesC struct {
+	ResultCCCC []struct {
+		Code   string  `json:"code"`
+		Close  float64 `json:"close"`
+		Volume int64   `json:"volume"`
+	} `json:"result"`
+}
+
 func main() {
-	var res []VolumeClose
-	a := "asfdasfsafds"
 	arr := append([]string{}, "2330", "2609")
 	tmp := UpdateVolumeArrBody{
 		StockNumArr: arr,
 	}
 	client := resty.New()
 	resp, err := client.R().
-		SetBody(&tmp).SetResult(&a).
-		Post("http://tradebot.tocandraw.com:3333/basic/volumeclos")
+		SetBody(&tmp).
+		SetResult([]SinoPacStockVolumeClose{}).
+		Post("http://127.0.0.1:3333/basic/volumeclose")
 	if err != nil {
 		fmt.Println(err)
 	}
-	// if err := json.Unmarshal(resp.Body(), &res); err != nil {
-	// 	panic(err)
-	// }
-	b := resp.Result().(*string)
-	fmt.Println(resp.StatusCode(), *b)
-	fmt.Println(res)
+	fmt.Println(resp.Result())
+	for _, v := range *resp.Result().(*[]SinoPacStockVolumeClose) {
+		fmt.Println(v)
+	}
 }
