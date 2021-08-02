2fa957a7c305f03d1d7db4973db8c6e4e7171dea
TimHsu@M1BP <maochindada@gmail.com>
2021-08-03 01:58:38 +0800

    1

--- T007/main.go
+++ T007/main.go
@@ -43,7 +43,7 @@ type AuthSucesC struct {
 }
 
 func main() {
-	arr := append([]string{}, "233ddssddddddddddddd0", "260d9")
+	arr := append([]string{}, "233ddssddddddddddddddd0", "260d9")
 	tmp := UpdateVolumeArrBody{
 		StockNumArr: arr,
 	}
